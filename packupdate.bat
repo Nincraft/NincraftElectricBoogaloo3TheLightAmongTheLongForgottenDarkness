5836c94f827f164510df64a072445ab4ce926541
Nincodedo <nincodedo@gmail.com>
2016-02-19 00:20:34 -0600

    Update to latest ModPack Downloader options

--- packupdate.bat
+++ packupdate.bat
@@ -1,4 +1,4 @@
 for /f "delims=" %%i IN ('dir *.jar /b') DO set modpackdownloader=%%i
-java -jar "%modpackdownloader%" all/mods.json all/mods -updateMods -mcVersion1.8.9 -releaseTyperelease
-java -jar "%modpackdownloader%" client/mods.json client/mods -updateMods -mcVersion1.8.9 -releaseTyperelease
+java -jar "%modpackdownloader%" all/mods.json all/mods -updateMods -mcVersion=1.8.9 -releaseType=release
+java -jar "%modpackdownloader%" client/mods.json client/mods -updateMods -mcVersion=1.8.9 -releaseType=release
 pause
